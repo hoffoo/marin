86141027d417f9856517678e6b7d379b848787c2
Marin <marin@stayke.net>
2014-07-18 15:53:14 -0700

    util

--- util.go
+++ util.go
@@ -6,16 +6,24 @@ import (
     "fmt"
 )
 
+// panic if err is not nil
 func PANIC_ON_ERR(err error) {
     if err != nil {
         panic(err)
     }
 }
 
+// a debug function to print varidic printf aruments
 func MSG(f string, v ...interface{}) {
     if len(v) == 0 {
-        fmt.Println(v)
+        fmt.Println(f)
     } else {
         fmt.Printf(f+"\n", v)
     }
+}
+
+// a debug function for prototyping: make go not complain about
+// unused variables
+func USE(i ...interface{}) {
+    return
 }
